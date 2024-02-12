1be21a07c9a460de77a2dae4a1687a6f4810a51a
Ogunbayik <ogun.bayikk@gmail.com>
2024-02-12 23:15:34 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private const string HORIZONTAL_INPUT = "Horizontal";
+    private const string VERTICAL_INPUT = "Vertical";
+
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private Transform body;
+
+    private float horizontalInput;
+    private float verticalInput;
+    private Vector3 movementDirection;
+
+    private bool isMove;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis(HORIZONTAL_INPUT);
+        verticalInput = Input.GetAxis(VERTICAL_INPUT);
+
+        movementDirection = new Vector3(horizontalInput, 0f, verticalInput);
+        movementDirection.Normalize();
+
+        if (movementDirection != Vector3.zero)
+            isMove = true;
+        else
+            isMove = false;
+
+        if (isMove)
+        {
+            transform.Translate(movementDirection * movementSpeed * Time.deltaTime);
+            HandleRotation();
+        }
+
+    }
+
+    private void HandleRotation()
+    {
+        if (movementDirection != Vector3.zero)
+        {
+            var rotationY = Quaternion.LookRotation(movementDirection, Vector3.up);
+            body.transform.rotation = Quaternion.RotateTowards(body.transform.rotation, rotationY, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
